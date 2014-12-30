0bdeb17002537ad8a12179bc4c09ff176a088633
john kyony <jkyony@gmail.com>
2014-12-30 16:09:25 +0200

    Fix the certificate bug

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,7 +234,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-     config.omniauth :facebook , Rails.application.config.facebook_id, Rails.application.config.facebook_key
+     config.omniauth :facebook , "562109160602052" , "4742fb1d41b89f4d731484c29eacf3e9"
+      :client_options => {:ssl => {:ca_path => '/usr/lib/ssl/certs/ca-certificates.crt'}}
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
