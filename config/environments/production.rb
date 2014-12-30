2a3ea1c426bb34828baf63e3af0c9893aed1e892
john kyony <jkyony@gmail.com>
2014-12-30 16:01:04 +0200

    Implement enviromental keys for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.facebook_id = ENV['facebook_id']
+  config.facebook_key = ENV['facebook_key']
 end
