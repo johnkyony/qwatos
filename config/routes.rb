4f170c01766139fb0b630c5d6652053877f38e1e
john kyony <jkyony@gmail.com>
2014-12-30 15:43:43 +0200

    Fix facebook bug

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
   resources :admin_panel
   devise_for :users , :controllers => {:omniauth_callbacks => "user/omniauth_callbacks"}
 
-  devise_scope :user do
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
   
   resources :qwatos
 
