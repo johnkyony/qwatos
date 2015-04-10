5dc9245cb26395a0bfa2306239d7898f3c128c16
john  kyony <jkyony@gmail.com>
2015-04-10 18:25:11 +0000

    add the google anylytics

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -1,6 +1,6 @@
 class TrendingController < ApplicationController
   def index
-     likes_count_id = Like.all.order(created_at: :asc).pluck(:qwato_id)
+     likes_count_id = Like.all.order(created_at: desc:).pluck(:qwato_id)
     qwato = Qwato.where(id: likes_count_id).pluck(:id)
     @qwatos = Qwato.where(id: qwato).shuffle
   end
