8b849d2d7771c48bdfc1f7eaaddbdb2f15ea8a6a
john kyony <jkyony@gmail.com>
2015-04-10 10:17:06 +0200

    the fix of the landing page

--- app/controllers/qwatos_controller.rb
+++ app/controllers/qwatos_controller.rb
@@ -2,9 +2,7 @@ class QwatosController < ApplicationController
   
  
   def index
-    likes_count_id = Like.all.order(id: :desc).pluck(:qwato_id)
-    qwato = find_by(id: likes_count_id)
-    @qwatos = Qwato.all.order(id: likes_count_id , "RANDOM")
+    @qwatos = Qwato.all.order(create_at: :desc)
     
   end
 
