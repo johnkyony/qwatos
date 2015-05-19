c7eb79bbcbc618baad865ee7bf9ffc093abb10a4
john  kyony <jkyony@gmail.com>
2015-05-19 15:53:38 +0000

    rap flows

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -4,7 +4,7 @@ class AdminPanelController < ApplicationController
     # redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
     @artist = Artist.all
     # code for the hashtag view on tumblr
-    client = Tumblr::Client.new({
+    @client = Tumblr::Client.new({
         :consumer_key => 'LdyvjSqikz1dpZdt73FzYssRpsyHIr6IwER4KMv8STB9A8eFBD',
         :consumer_secret => 'nJCPWEEUATPFVpp34iPVcSMxIOwLs7zXU6YiCulD9MLlL0vfZt',
         :oauth_token => '99BY5QP8ARK1KSCn6e0A5sEcnW7qA821t49N0i5I10xHWdfiKY',
@@ -12,7 +12,7 @@ class AdminPanelController < ApplicationController
       })
 
 
-    @artist_qwato_pool = client.tagged 'hip hop qoutes ', :filter => 'text'
+    @artist_qwato_pool = client.tagged 'hip hop qoutes ', :type => 'photo'
    
   end
 end
