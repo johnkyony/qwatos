bb69c969c6fe587992d99d5157b8ae564e4ee189
john  kyony <jkyony@gmail.com>
2015-05-19 23:11:18 +0000

    the push back to normal

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -4,15 +4,15 @@ class AdminPanelController < ApplicationController
     # redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
     @artist = Artist.all
     # code for the hashtag view on tumblr
-    @client = Tumblr::Client.new({
-        :consumer_key => 'LdyvjSqikz1dpZdt73FzYssRpsyHIr6IwER4KMv8STB9A8eFBD',
-        :consumer_secret => 'nJCPWEEUATPFVpp34iPVcSMxIOwLs7zXU6YiCulD9MLlL0vfZt',
-        :oauth_token => '99BY5QP8ARK1KSCn6e0A5sEcnW7qA821t49N0i5I10xHWdfiKY',
-        :oauth_token_secret => '44n5psrzAAWbtgKjJVLISsRE2wD9ainxCyR8Kli0NGwpAPpAd4'
-      })
+    client = Tumblr::Client.new({
+      :consumer_key => 'LdyvjSqikz1dpZdt73FzYssRpsyHIr6IwER4KMv8STB9A8eFBD',
+      :consumer_secret => 'nJCPWEEUATPFVpp34iPVcSMxIOwLs7zXU6YiCulD9MLlL0vfZt',
+      :oauth_token => '99BY5QP8ARK1KSCn6e0A5sEcnW7qA821t49N0i5I10xHWdfiKY',
+      :oauth_token_secret => '44n5psrzAAWbtgKjJVLISsRE2wD9ainxCyR8Kli0NGwpAPpAd4'
+    })
 
 
-    @artist_qwato_pool = client.tagged 'hip hop qoutes ', :type => 'photo'
-   
+    @artist_qwato_pool = client.posts("drake", :type => "photo")
+     @artist_qwato_pool = @artist_qwato_pool["posts"]
   end
 end
