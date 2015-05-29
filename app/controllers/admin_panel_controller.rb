d63c1ef95b77157153558685763be7a1b731853b
john  kyony <jkyony@gmail.com>
2015-05-29 21:19:09 +0000

    the tumblr fix

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -13,6 +13,6 @@ class AdminPanelController < ApplicationController
 
 
     @artist_qwato_pool = client.posts("drake", :type => "photo")
-     @artist_qwato_pool = @artist_qwato_pool["posts"]
+    
   end
 end
