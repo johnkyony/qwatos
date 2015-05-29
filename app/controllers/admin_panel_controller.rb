17ee454cfdc6ab0f36d79d36c6d8231c607551cf
john  kyony <jkyony@gmail.com>
2015-05-29 21:22:12 +0000

    the fix of the admin

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -1,10 +1,11 @@
+require "tumblr_client"
 class AdminPanelController < ApplicationController
   before_filter :authenticate_user!
   def index
     # redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
     @artist = Artist.all
     # code for the hashtag view on tumblr
-    client = Tumblr::Client.new({
+    @client = Tumblr::Client.new({
       :consumer_key => 'LdyvjSqikz1dpZdt73FzYssRpsyHIr6IwER4KMv8STB9A8eFBD',
       :consumer_secret => 'nJCPWEEUATPFVpp34iPVcSMxIOwLs7zXU6YiCulD9MLlL0vfZt',
       :oauth_token => '99BY5QP8ARK1KSCn6e0A5sEcnW7qA821t49N0i5I10xHWdfiKY',
@@ -12,7 +13,7 @@ class AdminPanelController < ApplicationController
     })
 
 
-    @artist_qwato_pool = client.posts("drake", :type => "photo")
+    @artist_qwato_pool = @client.posts("drake", :type => "photo")
     
   end
 end
